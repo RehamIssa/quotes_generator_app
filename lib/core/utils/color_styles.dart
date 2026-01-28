49908bb6b4242fb9531ee03cc6d73da47084774d
RehamIssa <reham.eissa1234@gmail.com>
2026-01-29 02:20:30 +0300

    creating the font and text styles

---
+++ lib/core/utils/color_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorStyles {
+  static const primaryFontColor = Color(0xff323232);
+  static const secondaryFontColor = Color(0xff6F6F6F);
+
+  static const primaryColor = Color(0xff8249B5);
+  static const secondaryColor = Color(0xffD0BCF0);
+}
---
+++ lib/core/utils/font_styels.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:quotes_generator_app/core/utils/color_styles.dart';
+
+class FontStyles {
+  static const fontStyle26 = TextStyle(
+    fontSize: 26,
+    color: ColorStyles.primaryFontColor,
+  );
+  static const fontStyle22 = TextStyle(
+    fontSize: 22,
+    color: ColorStyles.secondaryFontColor,
+  );
+  static const fontStyle20 = TextStyle(
+    fontSize: 20,
+    color: ColorStyles.primaryFontColor,
+  );
+}
