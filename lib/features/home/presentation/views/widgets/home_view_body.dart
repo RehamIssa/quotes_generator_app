995d282b7280b0eedd89ef8eba483d6932cc8188
RehamIssa <reham.eissa1234@gmail.com>
2026-01-29 15:40:48 +0300

    home view body done

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,64 +1,140 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:quotes_generator_app/core/utils/color_styles.dart';
-import 'package:quotes_generator_app/core/utils/font_styels.dart';
+import 'package:quotes_generator_app/core/utils/font_styles.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 10),
-            child: Stack(
-              alignment: AlignmentGeometry.topRight,
-              clipBehavior: Clip.none,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: ColorStyles.secondaryColor,
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(6),
-                      topLeft: Radius.circular(6),
-                    ),
-                  ),
-                  height: 60,
-                  width: double.infinity,
-                  child: Center(
-                    child: const Text(
-                      'Click Here To View Favorite Quotes',
-                      style: FontStyles.fontStyle26,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: 45,
-                  child: Container(
-                    height: 32,
-                    width: 32,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      color: ColorStyles.primaryFontColor,
-                    ),
-                    child: Center(
-                      child: Text(
-                        '2',
-                        style: FontStyles.fontStyle22.copyWith(
-                          color: Colors.white,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () {},
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Stack(
+                  alignment: AlignmentGeometry.topRight,
+                  clipBehavior: Clip.none,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: ColorStyles.secondaryColor,
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(6),
+                        ),
+                      ),
+                      height: 60,
+                      width: double.infinity,
+                      child: Center(
+                        child: const Text(
+                          'Click Here To View Favorite Quotes',
+                          style: FontStyles.fontStyle26,
                         ),
                       ),
                     ),
-                  ),
+                    Positioned(
+                      bottom: 45,
+                      right: -15,
+                      child: Container(
+                        height: 32,
+                        width: 32,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          color: ColorStyles.primaryFontColor,
+                        ),
+                        child: Center(
+                          child: Text(
+                            '2',
+                            style: FontStyles.fontStyle22.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Gap(10),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(6)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  '“All I required to be happy was friendship and people I could admire.”',
+                  style: FontStyles.fontStyle26,
+                  textAlign: TextAlign.justify,
+                ),
+                Text('Christian Dior', style: FontStyles.fontStyle22),
+                const Gap(20),
+                Row(
+                  children: [
+                    SizedBox(
+                      height: 48,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: ColorStyles.primaryColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(6),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'Generate Another Quote',
+                          style: FontStyles.fontStyle20.copyWith(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Gap(10),
+                    Expanded(
+                      child: SizedBox(
+                        height: 48,
+                        child: IconButton.outlined(
+                          iconSize: 32,
+                          color: ColorStyles.primaryColor,
+                          style: IconButton.styleFrom(
+                            side: BorderSide(
+                              width: 2.0,
+                              color: ColorStyles.primaryColor,
+                            ),
+
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                bottomRight: Radius.circular(6),
+                              ),
+                            ),
+                          ),
+                          onPressed: () {},
+                          icon: Icon(Icons.favorite_border_rounded),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
